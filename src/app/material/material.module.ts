51c665a696d9dbb1bf9e9241aee5ba744d5ef51a
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-11-21 11:37:40 -0300

    Toolbar

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  exports: [
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
