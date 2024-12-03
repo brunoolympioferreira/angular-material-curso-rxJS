dbf36ab1efcc5209fe5f77c0d0abb7937d499961
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-12-03 16:19:44 -0300

    Progress Bar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
@@ -18,7 +20,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
   declarations: [],
   providers: [
---
+++ src/app/progress-spinner/progress-spinner.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProgressBarMode } from '@angular/material/progress-bar';
+import { concat, interval, map, take, takeWhile, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-progress-spinner',
+  templateUrl: './progress-spinner.component.html',
+  styleUrl: './progress-spinner.component.scss'
+})
+export class ProgressSpinnerComponent implements OnInit {
+  public loadPercent = 0;
+  public queryValue = 0;
+  public queryMode: ProgressBarMode = 'query';
+
+  ngOnInit(): void {
+    this.loadingProgress(500, 95)
+      .subscribe(i => this.loadPercent = i);
+
+    concat(
+      interval(3000)
+        .pipe(
+          take(1),
+          tap(_ => (this.queryMode = 'determinate'))
+        ),
+      this.loadingProgress(500, 100)
+    ).subscribe(i => this.queryValue = i);
+  }
+
+  loadingProgress(speed: number, takeUntil: number) {
+    return interval(speed)
+      .pipe(
+        map(i => i * 5),
+        takeWhile(i => i <= takeUntil)
+      )
+  }
+}
